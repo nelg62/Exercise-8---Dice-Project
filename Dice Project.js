95563bd6b4425267adc6459d479a498e389ef9c0
nelg62 <glen.harding.nz@gmail.com>
2024-02-23 15:41:10 +1300

    Create functions for other dice

--- Dice Project.js
+++ Dice Project.js
@@ -1,4 +1,29 @@
+// Function for rolling random number between 1-4
+function D4() {
+    document.getElementById('D4output').innerHTML = Math.floor(Math.random() * 4) + 1
+}
+
 // Function for rolling random number between 1-6
 function D6() {
     document.getElementById('D6output').innerHTML = Math.floor(Math.random() * 6) + 1
+}
+
+// Function for rolling random number between 1-8
+function D8() {
+    document.getElementById('D8output').innerHTML = Math.floor(Math.random() * 8) + 1
+}
+
+// Function for rolling random number between 1-10
+function D10() {
+    document.getElementById('D10output').innerHTML = Math.floor(Math.random() * 10) + 1
+}
+
+// Function for rolling random number between 1-12
+function D12() {
+    document.getElementById('D12output').innerHTML = Math.floor(Math.random() * 12) + 1
+}
+
+// Function for rolling random number between 1-20
+function D20() {
+    document.getElementById('D20output').innerHTML = Math.floor(Math.random() * 20) + 1
 }
