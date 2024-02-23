a61581cef4f13052bb441a1dc518030a707c70c7
nelg62 <glen.harding.nz@gmail.com>
2024-02-24 12:46:01 +1300

    shortened javascript code and tidy up todo add form and shorten html

--- Dice Project.js
+++ Dice Project.js
@@ -1,29 +1,20 @@
-// Function for rolling random number between 1-4
-function D4() {
-    document.getElementById('D4output').innerHTML = Math.floor(Math.random() * 4) + 1
+
+// rolling dice runction massed on value from HTML
+function everydice(D) {
+  document.getElementById(`D${D}output`).innerHTML =
+    Math.floor(Math.random() * D) + 1;
 }
 
-// Function for rolling random number between 1-6
-function D6() {
-    document.getElementById('D6output').innerHTML = Math.floor(Math.random() * 6) + 1
-}
-
-// Function for rolling random number between 1-8
-function D8() {
-    document.getElementById('D8output').innerHTML = Math.floor(Math.random() * 8) + 1
-}
-
-// Function for rolling random number between 1-10
-function D10() {
-    document.getElementById('D10output').innerHTML = Math.floor(Math.random() * 10) + 1
-}
-
-// Function for rolling random number between 1-12
-function D12() {
-    document.getElementById('D12output').innerHTML = Math.floor(Math.random() * 12) + 1
-}
-
-// Function for rolling random number between 1-20
-function D20() {
-    document.getElementById('D20output').innerHTML = Math.floor(Math.random() * 20) + 1
+// function for choice button with error checking min number 1 and max number 100
+function choice() {
+  let choicenumber = document.getElementById("choicenumber").value;
+  if (choicenumber < 1 || choicenumber > 100) {
+    document.getElementById("choiceoutput").innerHTML =
+      "please Choose a number between 1 and 100";
+  } else {
+    document.getElementById("choiceoutput").innerHTML =
+      Math.floor(
+        Math.random() * document.getElementById("choicenumber").value
+      ) + 1;
+  }
 }
