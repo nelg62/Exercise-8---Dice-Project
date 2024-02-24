e285dfd95c540fe528f50462736c00b7d530d406
nelg62 <glen.harding.nz@gmail.com>
2024-02-24 16:42:25 +1300

    added block text to complete later

--- Dice Project.js
+++ Dice Project.js
@@ -23,24 +23,36 @@ const diceobjects = [4,6,8,10,12,20]
 
 
 for (let i = 0; i < diceobjects.length; i++) {
-  let newdice = document.createElement('div');
-  let newdice2 = document.createElement('div');
-  let newdice3 = document.createElement('button');
-  let newdice4 = document.createElement('div')
-  let newdice5 = document.createElement('h1')
-  newdice.className = 'dicecontainer';
-  newdice2.className = 'dice';
-  newdice2.id = `dice${diceobjects[i]}`
-  newdice3.className = 'dicebtn';
-  newdice3.id = `D${diceobjects[i]}`
-  newdice3.value = `${diceobjects[i]}`
-  newdice4.className = 'diceoutput'
-  newdice4.id = `diceoutput${diceobjects[i]}`
-  newdice5.className = 'diceoutputtxt'
-  newdice5.id = `D${diceobjects[i]}output`
-  document.getElementById('diceouterbox').appendChild(newdice);
-  document.getElementById('diceouterbox').appendChild(newdice2);
-  document.getElementById(`dice${diceobjects[i]}`).appendChild(newdice3);
-  document.getElementById('diceouterbox').appendChild(newdice4);
-  document.getElementById(`diceoutput${diceobjects[i]}`).appendChild(newdice5);
+  document.getElementById('diceouterbox').innerHTML += `<div>TESt</div>`
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // let newdice = document.createElement('div');
+  // let newdice2 = document.createElement('div');
+  // let newdice3 = document.createElement('button');
+  // let newdice4 = document.createElement('div')
+  // let newdice5 = document.createElement('h1')
+  // newdice.className = 'dicecontainer';
+  // newdice2.className = 'dice';
+  // newdice2.id = `dice${diceobjects[i]}`
+  // newdice3.className = 'dicebtn';
+  // newdice3.id = `D${diceobjects[i]}`
+  // newdice3.value = `${diceobjects[i]}`
+  // newdice4.className = 'diceoutput'
+  // newdice4.id = `diceoutput${diceobjects[i]}`
+  // newdice5.className = 'diceoutputtxt'
+  // newdice5.id = `D${diceobjects[i]}output`
+  // document.getElementById('diceouterbox').appendChild(newdice);
+  // document.getElementById('diceouterbox').appendChild(newdice2);
+  // document.getElementById(`dice${diceobjects[i]}`).appendChild(newdice3);
+  // document.getElementById('diceouterbox').appendChild(newdice4);
+  // document.getElementById(`diceoutput${diceobjects[i]}`).appendChild(newdice5);
 }
+
+
